5a8f5d24d0cd659942c769d283f326bfc574bbcf
Dana Rogers <rogers.d.207@gmail.com>
2021-03-12 19:19:28 -0600

    Last push.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,25 +1,80 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
+// // Add console.log to check to see if our code is working.
+// console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([30, 30], 2);
+// // Create the map object with center at the San Francisco airport.
+// let map = L.map('mapid').setView([30, 30], 2);
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-vll/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-vll/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+// // Accessing the airport GeoJSON URL
+// let airportData = "https://github.com/Deelacole2/Mapping_Earthquakes/blob/Mapping_GeoJSON_Points/majorAirports.json";
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data).addTo(map);
+// });
+
+// // Create the map object with center at the San Francisco airport.
+// // let map = L.map('mapid', {
+// //     center: [40.7, -94.5],
+// //     zoom: 4
+// // });
+
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+ //Create a base layer that hold both maps.
+let baseMaps ={
+    Street: streets,
+    Dark: dark
+};
 
-// Then we add our 'graymap' tile layer to the map.
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://github.com/Deelacole2/Mapping_Earthquakes/blob/Mapping_GeoJSON_Points/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/Deelacole2/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+    L.geoJson(data, {
+        onEachFeature: function(features, layer){
+            layer.bindPopup("<h2>Airport code: " + features.properties.faa + "</h2><hr><h3>Airport name: " + features.properties.name +"</h3>")
+        }
+    })
+    .addTo(map);
 });
 
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJson(data).bindPopup("<h2> Airport code: " + Features.properties.faa + "</h2> <hr> <h3>Airport name: " + Features.properties.name +"</h3>")
+// .addTo(map);
